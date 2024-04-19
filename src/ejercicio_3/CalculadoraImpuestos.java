2a2a899b7a74ca2f43eaa0910da2410d44561d5f
Ortega <Ortega@DESKTOP-7D19EM5>
2024-04-19 13:38:51 +0200

    EJERCICIO 3 SOLUCIONADO

--- src/ejercicio_3/CalculadoraImpuestos.java
+++ src/ejercicio_3/CalculadoraImpuestos.java
@@ -2,40 +2,67 @@ package ejercicio_3;
 
 public class CalculadoraImpuestos {
 
+
+    public static final int IVA = 1;
+    public static final int ISR = 2;
+    public static final int IEPS = 3;
 	
-	
-	public double calcularImpuestoTotal(double ingresos, String tipoImpuesto) {
+	public double calcularImpuestoTotal(double ingresos, int tipoImpuesto) throws Exception {
 
 		double impuesto = 0;
 
-		if (tipoImpuesto.equals("IVA")) {
+		if (tipoImpuesto==IVA) {
 
-			if (ingresos < 10000) {
-				impuesto = ingresos * 0.10;
-			}
+			impuesto = CalcularIVA(ingresos);
+		}
+		
+		else if (tipoImpuesto==ISR) {
 
-			else if (ingresos >= 10000 && ingresos < 50000) {
-				impuesto = ingresos * 0.15;
-			}
-
-			else {
-				impuesto = ingresos * 0.20;
-			}
-		} else if (tipoImpuesto.equals("ISR")) {
-
-			if (ingresos < 50000) {
-				impuesto = ingresos * 0.20;
-			}
-
-			else {
-				impuesto = ingresos * 0.30;
-			}
-		} else if (tipoImpuesto.equals("IEPS")) {
-			impuesto = ingresos * 0.05;
+			impuesto = CalcularISR(ingresos);
+		} 
+		
+		else if (tipoImpuesto==IEPS) {
+			
+			impuesto =ingresos*0.05;
 		}
 
 		else {
-			System.out.println("Tipo de impuesto no válido");
+			
+			    throw new Exception("Tipo de impuesto no válido");
+			
+			    
+				
+			
+		}
+		return impuesto;
+	}
+
+	
+
+	private double CalcularISR(double ingresos) {
+		double impuesto;
+		if (ingresos < 50000) {
+			impuesto = ingresos * 0.20;
+		}
+
+		else {
+			impuesto = ingresos * 0.30;
+		}
+		return impuesto;
+	}
+
+	private double CalcularIVA(double ingresos) {
+		double impuesto;
+		if (ingresos < 10000) {
+			impuesto = ingresos * 0.10;
+		}
+
+		else if (ingresos >= 10000 && ingresos < 50000) {
+			impuesto = ingresos * 0.15;
+		}
+
+		else {
+			impuesto = ingresos * 0.20;
 		}
 		return impuesto;
 	}
