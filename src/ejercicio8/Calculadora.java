d202cae9bc22572c692c79142c2fb8db92b2b07f
Ortega <Ortega@DESKTOP-7D19EM5>
2024-04-30 10:05:13 +0200

    EJERCICIO 8 SOLUCIONADO

---
+++ src/ejercicio8/Calculadora.java
@@ -0,0 +1,55 @@
+package ejercicio8;
+
+public class Calculadora {
+
+	public int sumar(int operador1, int operador2) {
+
+		return operador1 + operador2;
+
+	}
+
+	public int restar(int minuendo, int sustraendo) {
+
+		return minuendo - sustraendo;
+
+	}
+
+	public int multiplicar(int factor1, int factor2) {
+
+		return factor1 * factor2;
+
+	}
+
+	public int dividir(int dividendo, int divisor) throws Exception {
+
+		if (divisor == 0) {
+
+			throw new Exception("Error: No se pued dividir por 0");
+
+		}
+
+		if (divisor == 0) {
+
+			System.out.println("Error: No se puede dividir por cero.");
+
+			return 0;
+
+		}
+
+		return dividendo / divisor;
+
+	}
+
+	public double calcularAreaCirculo(double radio) {
+
+		return Math.PI * radio * radio;
+
+	}
+
+	public double calcularVolumenCilindro(double radio, double altura) {
+
+		return calcularAreaCirculo(radio) * altura;
+
+	}
+
+}
---
+++ src/ejercicio8/Start.java
@@ -0,0 +1,35 @@
+package ejercicio8;
+
+public class Start {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+
+		 Calculadora calc = new Calculadora(); 
+
+		 
+
+	        System.out.println("Suma: " + calc.sumar(10, 5)); 
+
+	        System.out.println("Resta: " + calc.restar(10, 5)); 
+
+	        System.out.println("Multiplicación: " + calc.multiplicar(10, 5)); 
+
+	        System.out.println("División: " + calc.dividir(10, 5)); 
+
+	 
+
+	        double radio = 5.0; 
+
+	        double altura = 10.0; 
+
+	        double area = calc.calcularAreaCirculo(radio);
+	        
+	        double volumen = calc.calcularVolumenCilindro(radio, altura);
+
+	        System.out.println("Área del círculo: " + area); 
+
+	        System.out.println("Volumen del cilindro: " + volumen); 
+	}
+
+}
